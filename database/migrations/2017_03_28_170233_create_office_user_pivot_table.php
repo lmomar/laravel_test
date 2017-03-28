889386876d1b3532b1dd69fbc86890684d2a9e74
omar <omar@DESKTOP-O4EIU8J>
2017-03-28 18:02:43 +0100

    laravel test for learning

---
+++ database/migrations/2017_03_28_170233_create_office_user_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class App\ extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('office_user', function (Blueprint $table) {
+            $table->integer('office_id')->unsigned()->index();
+            $table->foreign('office_id')->references('id')->on('offices')->onDelete('cascade');
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->primary(['office_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('office_user');
+    }
+}
